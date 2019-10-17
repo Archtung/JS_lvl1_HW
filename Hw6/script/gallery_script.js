f05908bc420ba552663e0365e1e09a842231afc8
Tung <damtung96@gmail.com>
2019-10-17 16:33:18 +0300

    Update 2

--- Hw6/script/gallery_script.js
+++ Hw6/script/gallery_script.js
@@ -1,22 +1,33 @@
-let mainImg = document.querySelector ('#main')
-        let roulette = document.querySelector ('#roulette')
-        let gall = document.querySelector ('#gallery')
+let mainImg = document.querySelector('#main')
+let roulette = document.querySelector('#roulette')
+let gall = document.querySelector('#gallery')
+let prev = document.querySelector('#prev')
+let next = document.querySelector('#next')
+// function gallery() {
+//     let number = 8
+//     let nodeStr = ''
 
-        function gallery () {
-            let number = 8
-            let nodeStr = ''
+//     for (let i = 1; i <= number; i++) {
+//         nodeStr += `<img src="img/${i}.jpg">`
+//     }
+//     roulette.innerHTML = nodeStr
+// }
+// gallery()
 
-            for (let i = 1; i <= number; i++) {
-                nodeStr += `<img src="img/${i}.jpg">`
-            }
-            roulette.innerHTML = nodeStr
-        } 
-        gallery ()
-        
-        function clicker (e) {
-            if (e.target.parentElement.id === 'roulette') {
-                mainImg.src = e.target.src
-            } 
+for (var j = 1; j <= 8; j++) {
+        var newImage = document.createElement('img');
+        newImage.setAttribute('src', 'img/' + j + '.jpg');
+        roulette.appendChild(newImage);
+        newImage.onclick = function(e) {
+            var imgSrc = e.target.getAttribute('src');
+            displayImage(imgSrc);
         }
-        
-        gall.addEventListener ('click', clicker)
+    }
+
+    function displayImage(imgSrc) {
+        mainImg.setAttribute('src', imgSrc);
+    }
+next.onclick = function () {
+    
+}
+// gall.addEventListener('click', clicker)
