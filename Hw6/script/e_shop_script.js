2c605a1c9f3a07777a05fe5e40ea70293da35fcf
Tung <damtung96@gmail.com>
2019-10-20 03:29:22 +0300

    Update

--- Hw6/script/e_shop_script.js
+++ Hw6/script/e_shop_script.js
@@ -1,46 +1,45 @@
-const ITEMS = ['AMD Ryzen 3', 'Intel Core i3', 'AMD Ryzen 5', 'Intel Core i5', 'AMD Ryzen 7', 'Intel Core i7', 'AMD Ryzen 9', 'Intel Core i9 Coffee Lake Refresh','Intel Core i9 Skylake-X Refresh']
-const PRICES = [6000, 11600, 13500, 17500, 25000, 27000, 46800, 36000,145000]
-const IDS = ['YD2200C5FBBOX', 'BX80684I39350KF', '100-100000031BOX', 'BX80684I59600K', '100-100000071BOX', 'BX80684I79700K', '100-100000023BOX', 'BX80684I99900K','BX80673I99980X']
+const ITEMS = ['AMD Ryzen 3', 'Intel Core i3', 'AMD Ryzen 5', 'Intel Core i5', 'AMD Ryzen 7', 'Intel Core i7', 'AMD Ryzen 9', 'Intel Core i9 Coffee Lake Refresh', 'Intel Core i9 Skylake-X Refresh']
+const PRICES = [6000, 11600, 13500, 17500, 25000, 27000, 46800, 36000, 145000]
+const IDS = ['YD2200C5FBBOX', 'BX80684I39350KF', '100-100000031BOX', 'BX80684I59600K', '100-100000071BOX', 'BX80684I79700K', '100-100000023BOX', 'BX80684I99900K', 'BX80673I99980X']
+let cartElem = document.querySelector('.cart');
+let cartlinkElem = document.getElementById('cart-link');
+let cartopenElem = document.querySelector('.cart1');
+let productsDTO = createDTO()
 
-let productsDTO = createDTO ()
+function createDTO() {
+    let arr = []
+    for (let i = 0; i < ITEMS.length; i++) {
+        arr.push(createProduct(ITEMS[i], PRICES[i], IDS[i]))
+    }
+    return arr
+    e_shop.innerHTML = nodeStr
+}
 
-        function createDTO () {
-            let arr = []
-            let nodeStr = ''
-            for (let i = 0; i < ITEMS.length; i++) {
-                arr.push (createProduct (ITEMS[i], PRICES[i], IDS[i]))               
-            }
-            return arr
-        }
-        function createProduct (name, price, id) {
-            return {
-                name: name,
-                id: id,
-                price: price
-            }
-        }
+function createProduct(name, price, id) {
+    return {
+        name: name,
+        id: id,
+        price: price
+    }
+}
 
-        function sum () {
-            let sum = 0
-            productsDTO.forEach (el => {
-                sum += el.price
-            })
-            console.log (sum)
-            return sum
-        }
-
-        // function e_eshop(){
-        //     let nodeStr = ''
-
-        //     for (let j = 1; j <= 8; j++) {
-        //         nodeStr += createProduct()
-        //     }
-        //     e_shop.innerHTML = nodeStr
-        // }
-        // e_eshop()
+function sum() {
+    let sum = 0
+    productsDTO.forEach(el => {
+        sum += el.price
+    })
+    console.log(sum)
+    return sum
+}
 
 
-        
+document.write('<h2>Каталог</h2>');
+for (i = 0; i < productsDTO.length; i++) { document.write(productsDTO[i].name + ' — ' + productsDTO[i].price + ' руб</br>'); }
+
+cartElem.onclick = function() {
+    cartopenElem.classList.toggle('open');
+};
+
 /*let store = {
     catalog: [],
     cart: [],
--- Hw6/script/gallery_script.js
+++ Hw6/script/gallery_script.js
@@ -1,33 +1,28 @@
 let mainImg = document.querySelector('#main')
 let roulette = document.querySelector('#roulette')
 let gall = document.querySelector('#gallery')
-let prev = document.querySelector('#prev')
-let next = document.querySelector('#next')
-// function gallery() {
-//     let number = 8
-//     let nodeStr = ''
+let cur = 1
 
-//     for (let i = 1; i <= number; i++) {
-//         nodeStr += `<img src="img/${i}.jpg">`
-//     }
-//     roulette.innerHTML = nodeStr
-// }
-// gallery()
+function gallery() {
+    let number = 8
+    let nodeStr = ''
 
-for (var j = 1; j <= 8; j++) {
-        var newImage = document.createElement('img');
-        newImage.setAttribute('src', 'img/' + j + '.jpg');
-        roulette.appendChild(newImage);
-        newImage.onclick = function(e) {
-            var imgSrc = e.target.getAttribute('src');
-            displayImage(imgSrc);
-        }
+    for (let i = 1; i <= number; i++) {
+        nodeStr += `<img src="img/${i}.jpg">`
     }
-
-    function displayImage(imgSrc) {
-        mainImg.setAttribute('src', imgSrc);
-    }
-next.onclick = function () {
-    
+    roulette.innerHTML = nodeStr
 }
-// gall.addEventListener('click', clicker)
+gallery()
+
+function clicker(evt) {
+    if (evt.target.parentElement.id === 'roulette') {
+        mainImg.src = evt.target.src
+    } else if (evt.target.dataset.dir) {
+        if (cur === 1 && evt.target.dataset.dir === '-1') cur = 9
+        if (cur === 8 && evt.target.dataset.dir === '1') cur = 0
+
+        mainImg.src = `img/${cur += +evt.target.dataset.dir}.jpg`
+    }
+}
+
+gall.addEventListener('click', clicker)
